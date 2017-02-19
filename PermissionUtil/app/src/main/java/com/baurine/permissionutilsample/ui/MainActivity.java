fa281aa4840fb8362d6c0130f44ca3a3989ca011
baurine <2008.hbl@gmail.com>
2017-02-19 11:35:49 +0800

    Add write SD card permission request example

--- PermissionUtil/app/src/main/java/com/baurine/permissionutilsample/ui/MainActivity.java
+++ PermissionUtil/app/src/main/java/com/baurine/permissionutilsample/ui/MainActivity.java
@@ -1,18 +1,24 @@
 package com.baurine.permissionutilsample.ui;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.TextView;
 
+import com.baurine.permissionutil.PermissionUtil;
 import com.baurine.permissionutilsample.R;
 import com.baurine.permissionutilsample.presenter.LocationPresenter;
+import com.baurine.permissionutilsample.util.CheckPermissionUtil;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class MainActivity
         extends BaseActivity
         implements LocationPresenter.LocationView {
 
     private LocationPresenter locationPresenter;
-    private TextView tvLocation;
+    private TextView tvLocation, tvSaveResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,18 @@ public class MainActivity
     private void initViews() {
         tvLocation = (TextView) findViewById(R.id.tv_location);
         showLocationResult("unknown!");
+
+        tvSaveResult = (TextView) findViewById(R.id.tv_save_result);
+        showSaveResult("unknown!");
     }
 
     @Override
     public void showLocationResult(String locationResult) {
         tvLocation.setText(getString(R.string.your_location, locationResult));
+    }
+
+    private void showSaveResult(String saveResult) {
+        tvSaveResult.setText(getString(R.string.save_result, saveResult));
     }
 
     ////////////////////////////////////////////
@@ -39,6 +52,42 @@ public class MainActivity
             case R.id.btn_location:
                 locationPresenter.requestLocation(this);
                 break;
+            case R.id.btn_save_location:
+                saveLocation();
+                break;
+        }
+    }
+
+    private void saveLocation() {
+        CheckPermissionUtil.checkWriteSd(this,
+                new PermissionUtil.ReqPermissionCallback() {
+                    @Override
+                    public void onResult(boolean success) {
+                        if (success) {
+                            saveLocationToFile();
+                        } else {
+                            showSaveResult("disallowed!");
+                        }
+                    }
+                });
+    }
+
+    private void saveLocationToFile() {
+        String content = tvLocation.getText().toString();
+        String filePath =
+                Environment.getExternalStorageDirectory().getPath() + "/location.txt";
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(filePath);
+            out.println(content);
+            showSaveResult(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            showSaveResult(e.getLocalizedMessage());
+        } finally {
+            if (out != null) {
+                out.close();
+            }
         }
     }
 }
--- PermissionUtil/app/src/main/java/com/baurine/permissionutilsample/util/CheckPermissionUtil.java
+++ PermissionUtil/app/src/main/java/com/baurine/permissionutilsample/util/CheckPermissionUtil.java
@@ -12,6 +12,7 @@ import com.baurine.permissionutil.PermissionUtil;
 public class CheckPermissionUtil {
 
     private static final int LOCATION_PERMISSION_REQ_CODE = 200;
+    private static final int WRITE_SD_REQ_CODE = 201;
 
     public static void checkLocation(Activity activity,
                                      PermissionUtil.ReqPermissionCallback callback) {
@@ -20,6 +21,16 @@ public class CheckPermissionUtil {
                 LOCATION_PERMISSION_REQ_CODE,
                 "We need location permission to locate your position",
                 "We can't get your location without location permission",
+                callback);
+    }
+
+    public static void checkWriteSd(Activity activity,
+                                    PermissionUtil.ReqPermissionCallback callback) {
+        PermissionUtil.checkPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                WRITE_SD_REQ_CODE,
+                "We need write external storage permission to save your location to file",
+                "We can't save your location to file without storage permission",
                 callback);
     }
 }
